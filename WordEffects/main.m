5af07982baa8cf01c7cd713d1f2285e0450826fc
Nathan Wainwright <nathanwainwright@gmail.com>
2018-07-30 17:31:19 -0700

    added more switch case statements

--- WordEffects/main.m
+++ WordEffects/main.m
@@ -13,6 +13,19 @@ int main(int argc, const char * argv[]) {
         
         while (true) {
             
+            int userChoice = 0;
+            char userStringChoice[255];
+            NSMutableString *canadaString = [[NSMutableString alloc] initWithString:@""];
+            
+            
+            
+            printf("Input your choice (number): ");
+            fgets(userStringChoice, 255, stdin);
+            
+            userChoice = atoi(userStringChoice); // converts userInput string, into a usable integer HAS NO ERROR CHECKING
+            
+            NSLog(@"User Inputting: %d",userChoice);
+            
             // 255 unit long array of characters
             char inputChars[255];
             
@@ -31,6 +44,46 @@ int main(int argc, const char * argv[]) {
             
             // print out memory address used
             NSLog(@"Memory Address was: %p", &inputString);
+            
+            switch (userChoice) {
+                case 1:
+                    // UPPER CASE
+                    NSLog(@"UPPERCASED: %@", [inputString uppercaseString]);
+                    break;
+                
+                case 2:
+                    // LOWER CASE
+                    NSLog(@"lowercased: %@", [inputString lowercaseString]);
+
+                    break;
+                    
+                case 3:
+                    // Numberize
+                    NSLog(@"Numberize: %d", [inputString intValue]);
+                    break;
+               
+                case 4:
+                    // Canadianize
+                    
+                    [canadaString appendFormat:@"%@, eh?", inputString];
+                    
+                    NSLog(@"Canadian: %@", canadaString); //BUG ISSUES; WHY IS IT DROPPING APPEND STRING ON NEW LINE??
+                    break;
+                    
+                case 5:
+                    // Respond
+                    
+                    break;
+                    
+                case 6:
+                    // De Space It
+                    
+                    NSLog (@"De-Space-It: %@", [inputString stringByReplacingOccurrencesOfString:@" " withString:@"-"]);
+                    break;
+                    
+                default:
+                    break;
+            }
 
         
         
